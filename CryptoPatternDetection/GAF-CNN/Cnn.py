329b2325068d6f4351e4be6cd3be0e2cd69cdd79
frank.liu <700554+frankliu-x@users.noreply.github.com>
2022-02-11 00:10:52 -0500

    update cnn and main in GAF-CNN

--- CryptoPatternDetection/GAF-CNN/Cnn.py
+++ CryptoPatternDetection/GAF-CNN/Cnn.py
@@ -12,24 +12,28 @@ class CNN(object):
     def __init__(self, arr_x, arr_y):
         self.arr_x, self.arr_y = arr_x, arr_y
         self.X_train_image, self.y_train_label, self.X_test_image, self.y_test_label = None, None, None, None
-        self. y_trainOneHot, self.y_testOneHot = None, None
+        self.y_trainOneHot, self.y_testOneHot = None, None
         self.input_shape = None
         self.label_shape = None
         self.model = None
         self.train_history = None
-        
+
     def process(self):
-        self.X_train_image, self.X_test_image, self.y_train_label, self.y_test_label = train_test_split (self.arr_x, self.arr_y, test_size= 0.3, random_state = 42)
-        self. y_trainOneHot, self.y_testOneHot = np_utils.to_categorical(self.y_train_label), np_utils.to_categorical(self.y_test_label)
+        self.X_train_image, self.X_test_image, self.y_train_label, self.y_test_label = train_test_split(self.arr_x,
+                                                                                                        self.arr_y,
+                                                                                                        test_size=0.3,
+                                                                                                        random_state=42)
+        self.y_trainOneHot, self.y_testOneHot = np_utils.to_categorical(self.y_train_label), np_utils.to_categorical(
+            self.y_test_label)
         self.input_shape = self.X_train_image[0].shape
-        self.label_shape = self. y_trainOneHot.shape[1]
-        
+        self.label_shape = self.y_trainOneHot.shape[1]
+
     def plot_image(self, image):
         fig = plt.gcf()
         fig.set_size_inches(2, 2)
-        plt.imshow(image, cmap = 'binary')
+        plt.imshow(image, cmap='binary')
         plt.show()
-      
+
     def plot_images_labels_prediction(self, images, labels, prediction, idx, num=10):
         fig = plt.gcf()
         fig.set_size_inches(12, 14)
@@ -46,7 +50,7 @@ class CNN(object):
             ax.set_yticks([])
             idx += 1
         plt.show()
-    
+
     def show_train_history(self, train_history, train, validation):
         plt.figure()
         plt.plot(train_history.history[train])
@@ -56,74 +60,75 @@ class CNN(object):
         plt.xlabel('Epoch')
         plt.legend(['train', 'validation'], loc='upper left')
         plt.show()
-    
+
     def build(self):
         self.model = Sequential()
         self.model.add(Conv2D(filters=32,
-                         kernel_size=(2, 2),
-                         padding='same',
-                         input_shape = self.input_shape,
-                         activation='relu'))
+                              kernel_size=(2, 2),
+                              padding='same',
+                              input_shape=self.input_shape,
+                              activation='relu'))
         self.model.add(MaxPooling2D(pool_size=(2, 2)))
         self.model.add(Conv2D(filters=32,
-                         kernel_size=(2, 2),
-                         padding='same',
-                         activation='relu'))
+                              kernel_size=(2, 2),
+                              padding='same',
+                              activation='relu'))
         self.model.add(MaxPooling2D(pool_size=(2, 2)))
-        
+
         self.model.add(Dropout(0.25))
         self.model.add(Flatten())
         self.model.add(Dense(128, activation='relu'))
         self.model.add(Dropout(0.5))
-        self.model.add(Dense(self.label_shape, activation='softmax'))    
-        
+        self.model.add(Dense(self.label_shape, activation='softmax'))
+
         print(self.model.summary())
-        
+
     def train(self, split):
-        #adam = optimizers.Adam(lr=0.01, beta_1=0.9, beta_2=0.999, epsilon=None, decay=0.0, amsgrad=False)
+        # adam = optimizers.Adam(lr=0.01, beta_1=0.9, beta_2=0.999, epsilon=None, decay=0.0, amsgrad=False)
         self.model.compile(loss='categorical_crossentropy',
                            optimizer='adam',
                            metrics=['accuracy'])
-        self.train_history = self.model.fit(x = self.X_train_image,
-                                       y = self.y_trainOneHot,
-                                       validation_split = split,
-                                       epochs = 100,
-                                       batch_size = 300,
-                                       verbose = 2)
-        
+        self.train_history = self.model.fit(x=self.X_train_image,
+                                            y=self.y_trainOneHot,
+                                            validation_split=split,
+                                            epochs=100,
+                                            batch_size=300,
+                                            verbose=2)
+
     def show(self):
         self.show_train_history(self.train_history, 'loss', 'val_loss')
         self.show_train_history(self.train_history, 'accuracy', 'val_accuracy')
         score = self.model.evaluate(self.X_test_image, self.y_testOneHot)
         print('Score of the Testing Data: {}'.format(score))
-        prediction = self.model.predict_classes(self.X_test_image)
-        #self.plot_images_labels_prediction(self.X_test_image, self.y_test_label, prediction, idx=0)
-        #print(pd.crosstab(self.y_test_label, prediction, rownames=['label'], colnames=['predict']))
-               
+
+        predict_x = self.model.predict(self.X_test_image)
+        prediction = np.argmax(predict_x, axis=1)
+        # prediction = self.model.predict_classes(self.X_test_image)
+        self.plot_images_labels_prediction(self.X_test_image, self.y_test_label, prediction, idx=0)
+        print(pd.crosstab(self.y_test_label, prediction, rownames=['label'], colnames=['predict']))
+
     def save(self, filename):
         self.model.save(filename)
-        
-        
-        
+
+
 if __name__ == "__main__":
-    
     with open('x_arr', 'rb') as handle:
-        x_arr = pickle.load(handle)     
+        x_arr = pickle.load(handle)
     with open('y_arr', 'rb') as handle:
-        y_arr = pickle.load(handle)     
-    
+        y_arr = pickle.load(handle)
+
     with open('load_data_sp500', 'rb') as handle:
         load_data = pickle.load(handle)
     x_arr = load_data['data']
     y_arr = load_data['target']
-    
+
     model = CNN(x_arr, y_arr)
     model.process()
     model.build()
     model.train(0.2)
     model.show()
-    
-    #model.model.predict_classes(model.X_test_image)
+
+    # model.model.predict_classes(model.X_test_image)
 
 
 
--- CryptoPatternDetection/GAF-CNN/Main.py
+++ CryptoPatternDetection/GAF-CNN/Main.py
@@ -12,7 +12,7 @@ from keras.models import load_model
 
 
 class Main(object):
-    
+
     def __init__(self, target, rule, url_his, url_real, his_ls, real_ls, signal_ls, save_plot):
         self.target = target
         self.rule = rule
@@ -32,24 +32,26 @@ class Main(object):
             self.pattern_dict[j] = i
         self.pattern_dict[len(signal_ls)] = 'No Pattern'
         self.package_realtime = None
-        
-    def api_history(self):
-        api_his = Api_history(self.url_his, self.his_ls, self.target, self.rule)
-        self.data = api_his.detect_history()
-        api_his.summary_history()
-        
+
+    def api_history(self, filename):
+        # api_his = Api_history(self.url_his, self.his_ls, self.target, self.rule)
+        self.data = filename
+        # api_his.summary_history()
+
     def rule_based(self):
         Sig = Signal(self.data, self.signal_ls, self.save_plot)
         Sig.process()
         self.data_pattern = Sig.detect_all()
         Sig.summary()
-        
+
     def gasf(self):
         data_1D_pattern = pd.read_csv(self.data_pattern)
         gasf_arr = np.zeros((len(self.signal_ls) + 1, 30, 10, 10, 4))
         for i, j in zip(self.signal_ls, range(len(self.signal_ls))):
             gasf = util_gasf.detect(data_1D_pattern, i)
-            gasf_arr[j, :, :, :, :] = gasf[0:30, :, :, :]
+            if gasf.shape[0] == 0:
+                continue  # ingore for now
+            gasf_arr[j, :, :, :, :] = gasf[0:30, :, :, :]  # data balancing
         df = data_1D_pattern.copy()
         for i in self.signal_ls:
             df = df.loc[df[i] != 1]
@@ -57,15 +59,15 @@ class Main(object):
         gasf = util_gasf.detect(data_1D_pattern, 'n', df)
         gasf_arr[-1, :, :, :, :] = gasf[0:30, :, :, :]
         self.gasf_arr = 'gasf_arr_' + self.target
-        with open(self.gasf_arr, 'wb') as handle:    
+        with open(self.gasf_arr, 'wb') as handle:
             pickle.dump(gasf_arr, handle, protocol=pickle.HIGHEST_PROTOCOL)
-    
+
     def process_xy(self):
         with open(self.gasf_arr, 'rb') as handle:
             gasf_arr = pickle.load(handle)
         x_arr = np.zeros(((len(self.signal_ls) + 1), 30, 10, 10, 4))
         for i in range(len(self.signal_ls) + 1):
-            x_arr[i, :, :, :, :] = gasf_arr[i, 0:30, :, :, :]    
+            x_arr[i, :, :, :, :] = gasf_arr[i, 0:30, :, :, :]
         x_arr = gasf_arr.reshape((len(self.signal_ls) + 1) * 30, 10, 10, 4)
         y_arr = []
         for i in range(len(self.signal_ls) + 1):
@@ -76,7 +78,7 @@ class Main(object):
         self.load_data = 'load_data_' + self.target
         with open(self.load_data, 'wb') as handle:
             pickle.dump(load_data, handle, protocol=pickle.HIGHEST_PROTOCOL)
-    
+
     def cnn(self):
         with open(self.load_data, 'rb') as handle:
             load_data = pickle.load(handle)
@@ -89,11 +91,11 @@ class Main(object):
         model.show()
         self.load_model = 'CNN_' + self.target + '.h5'
         model.save(self.load_model)
-        
+
     def api_realtime(self):
-        api_real = Api_realtime(self.url_his, self.url_real, self.his_ls, self.real_ls) 
-        self.package_realtime = api_real.detect_real() 
-        
+        api_real = Api_realtime(self.url_his, self.url_real, self.his_ls, self.real_ls)
+        self.package_realtime = api_real.detect_real()
+
     def predict_realtime(self):
         model = load_model(self.load_model)
         if self.package_realtime != False:
@@ -112,49 +114,51 @@ class Main(object):
         else:
             print('Not in the transaction time')
             return False
-    
-    
+
+
 if __name__ == "__main__":
-     # 夏令轉冬令的轉折時間 
-     target = 'S&P500'
-     rule = '1D'
-     url_his = 'https://financialmodelingprep.com/api/v3/historical-price-full/index/^GSPC'
-     url_real = 'https://financialmodelingprep.com/api/v3/quote/^GSPC'
-     his_ls = ['date', 'open', 'high', 'low', 'close', 'volume']
-     real_ls = ['timestamp', 'open', 'dayHigh', 'dayLow', 'price']
-     signal_ls = ['MorningStar', 'EveningStar', 'BearishHarami', 'BullishHarami']
-     save_plot = False
-     
-     main = Main(target, rule, url_his, url_real, his_ls, real_ls, signal_ls, save_plot)
-     main.api_history()
-     main.rule_based()
-     main.gasf()
-     main.process_xy()
-     main.cnn()
-     main.api_realtime()
-     main.predict_realtime()
-     
-     
-    
-     
-     
-     
-     
-     
-     
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
+    # 夏令轉冬令的轉折時間
+    target = 'S&P500'
+    rule = '1D'
+    url_his = None
+    url_real = None
+    his_ls = ['date', 'open', 'high', 'low', 'close', 'volume']
+    real_ls = ['timestamp', 'open', 'dayHigh', 'dayLow', 'price']
+    # signal_ls = ['MorningStar', 'EveningStar', 'BearishHarami', 'BullishHarami']
+    signal_ls = ['MorningStar', 'EveningStar']
+    save_plot = True
+    file_name = 'ETHUSD_history.csv'
+
+    main = Main(target, rule, url_his, url_real, his_ls, real_ls, signal_ls, save_plot)
+    main.api_history(filename=file_name)
+    main.rule_based()
+    main.gasf()
+    main.process_xy()
+    main.cnn()
+    # main.api_realtime()
+    # main.predict_realtime()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
